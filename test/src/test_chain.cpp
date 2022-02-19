d2c938c1ab0b163b82b2a0656b73988ad8c0f3d2
jay-tux <sys.jonas@yahoo.com>
2022-02-19 15:22:47 +0100

    Added simple chain test

--- test/src/test_chain.cpp
+++ test/src/test_chain.cpp
@@ -4,3 +4,33 @@
 #include "sources.hpp"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+
+using namespace fpgen;
+
+template <typename T1, typename T2>
+std::ostream &operator<<(std::ostream &in, std::tuple<T1, T2> &other) {
+  return in << "{ " << std::get<0>(other) << ", " << std::get<1>(other) << " }";
+}
+
+TEST(chain, simple_chain) {
+  /*
+  Chain:
+  -> [0..] -> [5..] -> [5..13] -> [15...169]  \
+                                               -> {[15...169], ['h'...'p']}
+  -> [0..] -> [7..] -> [7..22] -> ['h'...'w'] /
+  */
+  auto gen =
+      map(take(drop(inc((size_t)0), 5), 9), [](size_t in) { return in * in; });
+
+  auto second = map(take(drop(inc((size_t)0), 7), 22),
+                    [](size_t in) { return (char)('a' + (in % 26)); });
+
+  size_t value = 5;
+  for (auto v : zip(gen, second)) {
+    EXPECT_EQ(value * value, std::get<0>(v));
+    EXPECT_EQ('a' + 2 + value, std::get<1>(v));
+    EXPECT_TRUE(value <= 13);
+    value++;
+  }
+  EXPECT_EQ(value, 14);
+}
