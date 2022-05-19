2af2fd287ff3f299c2798d9996cd3295761eaac4
jay-tux <sys.jonas@yahoo.com>
2022-05-19 21:54:00 +0200

    License, package metadata

--- conanfile.py
+++ conanfile.py
@@ -4,6 +4,17 @@ from conans import ConanFile
 class Fpgen(ConanFile):
     name = "fpgen"
     version = "1.0.1"
+    license = "MPL"
+    author = "jay-tux sys.jonas@yahoo.com"
+    url = "https://github.com/jay-tux/fpgen"
+    topics = (
+        "generators",
+        "coroutines",
+        "c++20",
+        "header-only",
+        "functional-programming",
+        "functional",
+    )
     # header only, so no settings/options
     exports_sources = "inc/*", "include/*"
     no_copy_source = True
